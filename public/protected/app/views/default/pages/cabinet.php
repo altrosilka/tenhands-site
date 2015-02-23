132ee41705ef85ccdde7ed74d7a1c96ee709eb0c
Egor M <me@megor.ru>
2015-02-23 18:47:23 +0600

    try

--- public/protected/app/views/default/pages/cabinet.php
+++ public/protected/app/views/default/pages/cabinet.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="UTF-8" />
-  <!--<meta http-equiv="content-type" content="text/html; charset=windows-1251" />-->
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
   <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
   <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
 
--- public/protected/app/views/default/utils/head.php
+++ public/protected/app/views/default/utils/head.php
@@ -4,53 +4,10 @@
 <head>
   <meta charset="UTF-8" />
   <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+  <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+  <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
   
   <link rel="stylesheet" type="text/css" href="/pack/site-vendor.css" />
   <link rel="stylesheet" type="text/css" href="/pack/site-styles.css" />
 
-
-  <title>Кто круче: Майкл Джордан или Кобе Брайант? - Записки латгальского француза - Блоги - Sports.ru</title>
-  <meta name="description" content="Блог «Записки латгальского француза» представляет сравнительную инфографику о карьерах двух великих защитников." />
-  <meta name="copyright" content="copyright © 2013, Sports.ru" />
-
-  <link rel="canonical" href="http://www.sports.ru/tribuna/blogs/cristianliberum/719920.html" />
-  <link rel="alternate" media="only screen and (max-width: 640px)" href="http://m.sports.ru/tribuna/blogs/cristianliberum/719920.html" />
-
-  <meta name="Robots" content="index, follow" />
-  <meta name="robots" content="all" />
-  <meta name="viewport" content="width=device-width" />
-  <meta name="format-detection" content="telephone=no" />
-  <meta name="apple-itunes-app" content="app-id=542339626" />
-  <meta name="google-play-app" content="app-id=ru.sports" />
-  <meta name="app-tag-name" content="Sports.ru" />
-  <meta name="app-icon" content="http://www.sports.ru/storage/simple/ru/edt/00/09/80/30/rue945729d5e8.113.150x150.png" />
-
-  <link rel="apple-touch-icon" size="76x76" href="http://www.sports.ru/apple-touch-icon-76.png" />
-  <link rel="apple-touch-icon" size="120x120" href="http://www.sports.ru/apple-touch-icon-120.png" />
-  <link rel="apple-touch-icon" size="152x152" href="http://www.sports.ru/apple-touch-icon-152.png" />
-  <link rel="apple-touch-icon" size="1024x1024" href="http://www.sports.ru/apple-touch-icon-1024.png" />
-  <link rel="apple-touch-icon" href="http://www.sports.ru/apple-touch-icon-1024.png" />
-  <link rel="Shortcut Icon" type="image/x-icon" href="http://www.sports.ru/favicon.ico" />
-  <link rel="image_src" href="http://s5o.ru/storage/simple/ru/edt/55/35/88/53/rue875cb04bc0.jpg" />
-
-  <meta name="twitter:card" content="summary" />
-  <meta name="twitter:site" content="@sportsru" />
-  <meta name="twitter:url" content="http://www.sports.ru/tribuna/blogs/cristianliberum/719920.html" />
-  <meta name="twitter:title" content="Кто круче: Майкл Джордан или Кобе Брайант? - Записки латгальского француза - Блоги - Sports.ru" />
-  <meta name="twitter:description" content="Блог «Записки латгальского француза» представляет сравнительную инфографику о карьерах двух великих защитников." />
-  <meta name="twitter:image" content="http://s5o.ru/storage/simple/ru/edt/55/35/88/53/rue875cb04bc0.jpg" />
-  <meta name="twitter:app:url:googleplay" content="sports://blog_post/719920" />
-  <meta name="twitter:app:url:iphone" content="sports://blog_post/719920" />
-  <meta name="twitter:app:url:ipad" content="sports://blog_post/719920" />
-  <meta name="twitter:app:name:googleplay" content="Sports.ru" />
-  <meta name="twitter:app:name:iphone" content="Sports.ru" />
-  <meta name="twitter:app:name:ipad" content="Sports.ru" />
-
-  <meta property="og:type" content="article" />
-  <meta property="og:site_name" content="Sports.ru" />
-  <meta property="og:title" content="Кто круче: Майкл Джордан или Кобе Брайант? - Записки латгальского француза - Блоги - Sports.ru" />
-  <meta property="og:description" content="Блог «Записки латгальского француза» представляет сравнительную инфографику о карьерах двух великих защитников." />
-  <meta property="og:image" content="http://s5o.sports.ru/storage/simple/ru/edt/55/35/88/53/rue875cb04bc0.jpg" />
-
-  <meta property="fb:app_id" content="109556845805232" />
-  <meta property="fb:page_id" content="110179310119" /> 
+  <title>Десять Рук - размещение и аналитика в социальных сетях</title>
